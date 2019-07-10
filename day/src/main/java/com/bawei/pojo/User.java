9140eb7207ed537f72d803d5a9e6c8e1ed4618f1
long201013 <2237944021@qq.com>
2019-07-10 21:51:11 +0800

    sss

---
+++ day/src/main/java/com/bawei/pojo/User.java
@@ -0,0 +1,15 @@
+package com.bawei.pojo;
+
+
+public class User {
+public static void main(String[] args) {
+	System.out.println(123000);
+	System.out.println(123123);
+	System.out.println(123123);
+	System.out.println(123456);
+	System.out.println(123456);
+	System.out.println(123456);
+	int i=1;
+	
+}
+}
